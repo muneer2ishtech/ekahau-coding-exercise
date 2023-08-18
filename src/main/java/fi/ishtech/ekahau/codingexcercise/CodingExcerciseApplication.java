4d3786a7500853b0eb9e6ade1af33ff7ce8b961f
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-08-18 18:13:42 +0300

    Project skeleton

---
+++ src/main/java/fi/ishtech/ekahau/codingexcercise/CodingExcerciseApplication.java
@@ -0,0 +1,13 @@
+package fi.ishtech.ekahau.codingexcercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingExcerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingExcerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/ishtech/ekahau/codingexcercise/CodingExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.ishtech.ekahau.codingexcercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingExcerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
