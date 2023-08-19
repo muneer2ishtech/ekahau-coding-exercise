da094db12b53686bb67178f6480fd26dd642c0c0
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-08-19 18:58:54 +0300

    userrepo.findOneByEmail

--- src/main/java/fi/ishtech/ekahau/codingexcercise/repo/UserRepo.java
+++ src/main/java/fi/ishtech/ekahau/codingexcercise/repo/UserRepo.java
@@ -1,5 +1,7 @@
 package fi.ishtech.ekahau.codingexcercise.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fi.ishtech.ekahau.codingexcercise.entity.User;
@@ -9,5 +11,7 @@ import fi.ishtech.ekahau.codingexcercise.entity.User;
  * @author Muneer Ahmed Syed
  */
 public interface UserRepo extends JpaRepository<User, Long> {
+
+	Optional<User> findOneByEmail(String email);
 
 }
