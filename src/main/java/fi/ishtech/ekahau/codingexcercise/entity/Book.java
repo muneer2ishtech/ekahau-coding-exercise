059151032b59d2a4c88f2a041be4169216868beb
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-08-19 11:42:42 +0300

    tables for user and book

---
+++ src/main/java/fi/ishtech/ekahau/codingexcercise/entity/Book.java
@@ -0,0 +1,43 @@
+package fi.ishtech.ekahau.codingexcercise.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "t_book")
+@DynamicInsert
+@DynamicUpdate
+@Data
+public class Book implements Serializable {
+
+	private static final long serialVersionUID = -4073976845697599055L;
+
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String title;
+
+	@Column(nullable = false)
+	private String author;
+
+	@Column(nullable = false, precision = 4, columnDefinition = "SMALLINT")
+	private Integer year;
+
+	@Column(nullable = false, precision = 10, scale = 2)
+	private BigDecimal price;
+
+}
