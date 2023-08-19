c694ab95be7611472936cdc0c372b7907d36770f
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-08-19 16:36:33 +0300

    use of reponse entity to handle not found

--- src/main/java/fi/ishtech/ekahau/codingexcercise/controller/BookController.java
+++ src/main/java/fi/ishtech/ekahau/codingexcercise/controller/BookController.java
@@ -3,6 +3,7 @@ package fi.ishtech.ekahau.codingexcercise.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,8 +28,8 @@ public class BookController {
 	}
 
 	@GetMapping("/api/v1/books/{id}")
-	Book findById(@PathVariable Long id) {
-		return bookRepo.findById(id).orElse(null);
+	public ResponseEntity<Book> findById(@PathVariable Long id) {
+		return ResponseEntity.of(bookRepo.findById(id));
 	}
 
 }
