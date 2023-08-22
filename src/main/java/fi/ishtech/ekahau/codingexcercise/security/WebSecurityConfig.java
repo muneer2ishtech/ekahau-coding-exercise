45ce3e1d4c97a80ce11836bc8f573256b4fa6595
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-08-22 14:13:59 +0300

    cors config

--- src/main/java/fi/ishtech/ekahau/codingexcercise/security/WebSecurityConfig.java
+++ src/main/java/fi/ishtech/ekahau/codingexcercise/security/WebSecurityConfig.java
@@ -1,11 +1,16 @@
 package fi.ishtech.ekahau.codingexcercise.security;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +19,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import fi.ishtech.ekahau.codingexcercise.security.userdetails.UserDetailsServiceImpl;
 
@@ -55,10 +63,23 @@ public class WebSecurityConfig {
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable())
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**", "/error", "/", "/favicon.ico")
-						.permitAll().anyRequest().authenticated());
+		// @formatter:off
+ 		http
+ 			.csrf(csrf -> csrf.disable())
+ 			.cors(Customizer.withDefaults())
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(
+					auth -> auth
+						.requestMatchers(
+								"/api/auth/**",
+								"/error",
+								"/",
+								"/favicon.ico"
+						)
+							.permitAll()
+						.anyRequest()
+							.authenticated());
+		// @formatter:on
 
 		http.authenticationProvider(authenticationProvider());
 
@@ -67,4 +88,21 @@ public class WebSecurityConfig {
 		return http.build();
 	}
 
+	/**
+	 * Creates {@link CorsConfiguration}<br>
+	 * Defaults are allowedOrigins = *, allowedHeaders = *, maxAge = 1800<br>
+	 * You can use addAllowedOriginPattern to set http and/or https, subdomains etc.
+	 * 
+	 */
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.applyPermitDefaultValues();
+		corsConfig.setAllowedMethods(
+				Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
+		UrlBasedCorsConfigurationSource corsConfigSource = new UrlBasedCorsConfigurationSource();
+		corsConfigSource.registerCorsConfiguration("/**", corsConfig);
+
+		return corsConfigSource;
+	}
 }
