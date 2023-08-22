b6a7336e48a908d01f245c30710ec14ad0805e01
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-08-22 03:15:26 +0300

    unique constraints in tables

--- src/main/resources/db/migration/V20230819_120338__create_table_user.sql
+++ src/main/resources/db/migration/V20230819_120338__create_table_user.sql
@@ -1,6 +1,6 @@
 CREATE TABLE t_user (
   id             BIGSERIAL     NOT NULL,
-  email          VARCHAR(255)  NOT NULL,
+  email          VARCHAR(255)  NOT NULL UNIQUE,
   password_hash  VARCHAR(255)  NOT NULL,
   first_name     VARCHAR(255)  NOT NULL,
   last_name      VARCHAR(255)  NOT NULL,
