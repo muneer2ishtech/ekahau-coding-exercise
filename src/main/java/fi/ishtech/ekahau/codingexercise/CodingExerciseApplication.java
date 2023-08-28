04d407cffe7b364fa02244734071eb9045415fc8
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-08-28 17:25:36 +0300

    Fix name from excercise to exercise

--- src/main/java/fi/ishtech/ekahau/codingexercise/CodingExerciseApplication.java
+++ src/main/java/fi/ishtech/ekahau/codingexercise/CodingExerciseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodingExcerciseApplication {
+public class CodingExerciseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodingExcerciseApplication.class, args);
+		SpringApplication.run(CodingExerciseApplication.class, args);
 	}
 
 }
