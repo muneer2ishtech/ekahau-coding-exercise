2295dbede5720421e224ad9f3d9b546312fc3448
Muneer Ahmed Syed <muneer@ishtech.fi>
2023-08-28 17:23:53 +0300

    rename package to exercise

---
+++ src/main/java/fi/ishtech/ekahau/codingexercise/CodingExcerciseApplication.java
@@ -0,0 +1,13 @@
+package fi.ishtech.ekahau.codingexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingExcerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingExcerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/ishtech/ekahau/codingexercise/CodingExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.ishtech.ekahau.codingexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingExcerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
